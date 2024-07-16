93c87b95976d3dd6448e9d03ef08f5d252249aa0
Brandon <brandon@tendency.me>
2024-07-16 00:59:04 -0500

    🧑‍💻 Add strict types to the `get_svg` helper

--- src/helpers.php
+++ src/helpers.php
@@ -9,16 +9,10 @@ use Log1x\SageSvg\SageSvg;
 use function Roots\app;
 
 /**
- * Return the inlined contents of an SVG if it exists.
- *
- * @param  string       $image
- * @param  string|array $class
- * @param  array        $attrs
- * @return string
+ * Render the specified SVG image if it exists.
  */
-
 if (! function_exists('get_svg')) {
-    function get_svg($image, $class = '', $attrs = [])
+    function get_svg(string $image, string|array $class = '', array $attrs = []): ?string
     {
         return app(SageSvg::class)->render($image, $class, $attrs);
     }
