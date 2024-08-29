c871fa875830b69ee751e97d1914a5c5458376d2
Sergi Arias <sergi@caramelconcept.com>
2024-08-29 16:11:41 +0200

    🩹 Fix missing `array` type for passing attachments (#29)

--- src/SageSvg.php
+++ src/SageSvg.php
@@ -28,7 +28,7 @@ class SageSvg
     /**
      * Render the SVG as HTML
      */
-    public function render(string $image, string|array $class = '', array $attrs = [], array $options = []): HtmlString
+    public function render(string|array $image, string|array $class = '', array $attrs = [], array $options = []): HtmlString
     {
         $options = collect([
             'idPrefix' => null,
--- src/helpers.php
+++ src/helpers.php
@@ -11,7 +11,7 @@ use Log1x\SageSvg\SageSvg;
  * Render the specified SVG image if it exists.
  */
 if (! function_exists('get_svg')) {
-    function get_svg(string $image, string|array $class = '', array $attrs = [], array $options = []): HtmlString
+    function get_svg(string|array $image, string|array $class = '', array $attrs = [], array $options = []): HtmlString
     {
         return app(SageSvg::class)->render($image, $class, $attrs, $options);
     }
